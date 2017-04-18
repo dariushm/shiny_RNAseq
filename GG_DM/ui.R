b94907bb530169c4ac3e2fd29d527960ec88b2fb
Dariush Mohammadyani <dariush@jhu.edu>
2017-04-18 10:41:12 -0400

    create new folder

---
+++ GG_DM/ui.R
@@ -0,0 +1,51 @@
+library(shiny)
+ui = fluidPage(
+  titlePanel("Monocle"),
+  sidebarLayout(
+    sidebarPanel(
+      # input matrix file
+      fileInput("matrix", "Expression Matrix", accept = c("text/csv", "text/comma-separated-values,text/plain", ".csv")),
+      tags$hr(),
+      checkboxInput('header', 'Header', TRUE),
+
+      # input phenodata and modify the titles
+      fileInput("phenodata", "Pheno Data", accept = c("text/csv", "text/comma-separated-values,text/plain", ".csv")),
+      tags$hr(),
+      checkboxInput('header', 'Header', TRUE),
+
+      # input featuredata and modify the titles
+      fileInput("featuredata", "Feature Data", accept = c("text/csv", "text/comma-separated-values,text/plain", ".csv")),
+      tags$hr(),
+      checkboxInput('header', 'Header', TRUE)
+    ),
+    mainPanel(
+      tabsetPanel(
+      tabPanel("Pheno",
+        splitLayout(
+          conditionalPanel(condition = "output.phload", uiOutput(paste0('pheno_col', 1)), div(style = "height:200px;")),
+          conditionalPanel(condition = "output.phload", uiOutput(paste0('pheno_col', 2)), div(style = "height:200px;")),
+          conditionalPanel(condition = "output.phload", uiOutput(paste0('pheno_col', 3)), div(style = "height:200px;")),
+          conditionalPanel(condition = "output.phload", uiOutput(paste0('pheno_col', 4)), div(style = "height:200px;")),
+          conditionalPanel(condition = "output.phload", uiOutput(paste0('pheno_col', 5)), div(style = "height:200px;")),
+          conditionalPanel(condition = "output.phload", uiOutput(paste0('pheno_col', 6)), div(style = "height:200px;")),
+          conditionalPanel(condition = "output.phload", uiOutput(paste0('pheno_col', 7)), div(style = "height:200px;")),
+          conditionalPanel(condition = "output.phload", uiOutput(paste0('pheno_col', 8)), div(style = "height:200px;"))
+        ),
+        tableOutput("pd")
+      ),
+      tabPanel("Feature",
+        splitLayout(
+          conditionalPanel(condition = "output.fdload", uiOutput(paste0('feature_col', 1)), div(style = "height:200px;")),
+          conditionalPanel(condition = "output.fdload", uiOutput(paste0('feature_col', 2)), div(style = "height:200px;")),
+          conditionalPanel(condition = "output.fdload", uiOutput(paste0('feature_col', 3)), div(style = "height:200px;")),
+          conditionalPanel(condition = "output.fdload", uiOutput(paste0('feature_col', 4)), div(style = "height:200px;")),
+          conditionalPanel(condition = "output.fdload", uiOutput(paste0('feature_col', 5)), div(style = "height:200px;")),
+          conditionalPanel(condition = "output.fdload", uiOutput(paste0('feature_col', 6)), div(style = "height:200px;")),
+          conditionalPanel(condition = "output.fdload", uiOutput(paste0('feature_col', 7)), div(style = "height:200px;")),
+          conditionalPanel(condition = "output.fdload", uiOutput(paste0('feature_col', 8)), div(style = "height:200px;"))
+        ),
+        tableOutput("fd")
+      )
+    )
+  )
+))
